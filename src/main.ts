3e9f1a479d18a3795853ea50b29eb9d819ac7643
VINICIUS WAGNER GOMES GERMANO <202051901276@alunos.unifbv.edu.br>
2025-02-01 15:52:24 -0300

    iniciando config do swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger"
 import { ValidationPipe } from "@nestjs/common"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder().setTitle('Amigo Secreto').
+  setDescription("API de Sorteio para Amigo Secreto").setVersion('0.1').addTag('amigosecreto').build()
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api',app, documentFactory)
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }))
   await app.listen(process.env.PORT || 3000)
